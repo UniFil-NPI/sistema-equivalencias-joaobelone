4b26dcd0646d67263e9120d8283cb1fac31f5345
beloner <joao.belone@edu.unifil.br>
2024-05-09 23:49:28 -0300

    scripts FIX

---
+++ sql-helper.py
@@ -0,0 +1,24 @@
+# for i in range(1,44 +1):
+#     if(i == 1):
+#         print(f'insert into disciplinas_grades (disciplinas_id, grades_id) values ({i}, 1),')
+#     else:
+#         print(f'({i}, 1),')
+
+# for i in range(44+1,102 +1):
+#     if(i == 1):
+#         print(f'insert into disciplinas_grades (disciplinas_id, grades_id) values ({i}, 2),')
+#     else:
+#         print(f'({i}, 2),')
+
+# for i in range(102+1,279+1):
+#     if(i == 1):
+#         print(f'insert into disciplinas_grades (disciplinas_id, grades_id) values ({i}, 3),')
+#     else:
+#         print(f'({i}, 3),')
+
+
+for i in range(279+1,295+1):
+    if(i == 1):
+        print(f'insert into disciplinas_grades (disciplinas_id, grades_id) values ({i},4),')
+    else:
+        print(f'({i},4),')
