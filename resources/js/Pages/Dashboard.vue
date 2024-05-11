edf6a44d1bb3e3b3cef99c034e203e25728cecd5
beloner <joao.belone@edu.unifil.br>
2024-05-11 11:38:18 -0300

    data table search - dashboard routes

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,31 +14,31 @@ import  DisciplinasSVG from "@/assets/svgs/DisciplinasSVG.vue";
         </template>
 
         <div class="flex justify-center">
-            <div class="sm:max-w-2xl justify-center grid grid-cols-2 gap-10">
+            <div class="sm:max-w-2xl justify-center grid grid-cols-2 gap-4">
                 <div class="py-12 col-span-2 sm:col-span-1">
                 <a href="#">
                     <div
-                        class="mb-4 hover:border-amber-600 border-2 border-transparent transition ease-in-out bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        class="hover:scale-105 mb-4 hover:border-primary border-4 border-transparent transition ease-in-out bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class=" p-6 text-gray-900 text-xl font-bold dark:text-gray-100">UIUI EXEMPLO EXEMPLO <DisciplinasSVG/></div>
                     </div>
                 </a>
             </div>
             <div class="py-12 col-span-2 sm:col-span-1">
-                <a href="#">
+                <a :href="route('disciplinas.index')">
                     <div
-                        class="mb-4 hover:border-amber-600 border-2 border-transparent transition ease-in-out bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        class="hover:scale-105 mb-4 hover:border-primary border-4 border-transparent transition ease-in-out bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class=" p-6 text-gray-900 text-xl font-bold dark:text-gray-100">Disciplinas  </div>
                     </div>
                 </a>
-                <a href="#">
+                <a :href="route('equivalencias.index')">
                     <div
-                        class="mb-4 hover:border-amber-600 border-2 border-transparent transition ease-in-out bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        class="hover:scale-105 mb-4 hover:border-primary border-4 border-transparent transition ease-in-out bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class=" p-6 text-gray-900 text-xl font-bold dark:text-gray-100">Equivalências</div>
                     </div>
                 </a>
-                <a href="#">
+                <a :href="route('grades.index')">
                     <div
-                        class="mb-4 hover:border-amber-600 border-2 border-transparent transition ease-in-out bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        class="hover:scale-105 hover:border-primary border-4 border-transparent transition ease-in-out bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class=" p-6 text-gray-900 text-xl font-bold dark:text-gray-100">Grades</div>
                     </div>
                 </a>
--- resources/js/Pages/Disciplinas/Index.vue
+++ resources/js/Pages/Disciplinas/Index.vue
@@ -1,7 +1,10 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import { onMounted } from 'vue';
+import InputText from 'primevue/inputtext';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import { onMounted, ref } from 'vue';
 import { usePage } from '@inertiajs/vue3';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
@@ -9,6 +12,7 @@ import Swal from 'sweetalert2/dist/sweetalert2.js'
 import 'sweetalert2/src/sweetalert2.scss'
 import axios from 'axios';
 import { toastMixin } from '@/utils/toast';
+import { FilterMatchMode } from 'primevue/api';
 
 const page = usePage();
 
@@ -21,6 +25,7 @@ const confirmDelete = async (id) => {
         showCancelButton: true,
         confirmButtonText: "Sim",
         cancelButtonText: 'Não',
+        confirmButtonColor: "#e98e06",
     });
 
     if (result.isConfirmed) {
@@ -36,6 +41,10 @@ const confirmDelete = async (id) => {
         Swal.fire("Changes are not saved", "", "info");
     }
 }
+
+const filters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS }
+});
 </script>
 <template>
 
@@ -46,7 +55,7 @@ const confirmDelete = async (id) => {
             <div class="flex justify-between">
                 <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Disciplinas</h2>
                 <a :href="route('disciplinas.create')"
-                    class="bg-amber-600 hover:bg-amber-800 text-white font-bold py-2 px-4 rounded-full">+ Criar
+                    class="bg-primary hover:bg-amber-800 text-white font-bold py-2 px-4 rounded-full">+ Criar
                     Disciplina</a>
             </div>
         </template>
@@ -59,8 +68,23 @@ const confirmDelete = async (id) => {
                         </div>
                     </div>
                     <div v-if="!empty">
-                        <DataTable stripedRows paginator :rows="25" :rowsPerPageOptions="[25, 50, 100]"
-                            :value="$page.props.disciplinas" tableStyle="min-width: 20rem">
+                        <DataTable v-model:filters="filters" filter removableSort stripedRows paginator :rows="25"
+                            :rowsPerPageOptions="[25, 50, 100]" :value="$page.props.disciplinas"
+                            tableStyle="min-width: 20rem"
+                            :globalFilterFields="['codigo', 'titulo', 'tipo', 'modalidade', 'carga_horaria']">
+                            <template #empty>Nenhuma disciplina encontrada.</template>
+                            <template #header>
+                                <div class="flex justify-start">
+                                    <IconField class="pt-2" iconPosition="left">
+                                        <InputIcon>
+                                            <i class="pi pi-search" />
+                                        </InputIcon>
+                                        <InputText
+                                            class="shadow appearance-none border rounded ps-10 w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-gray-300 dark:border-gray-600"
+                                            v-model="filters['global'].value" placeholder="Pesquisa" />
+                                    </IconField>
+                                </div>
+                            </template>
                             <Column field="codigo" header="Codigo" sortable></Column>
                             <Column field="titulo" header="Titulo" sortable></Column>
                             <Column field="carga_horaria" header="Carga Horaria" sortable></Column>
@@ -75,7 +99,7 @@ const confirmDelete = async (id) => {
                                 <template #body="slotProps">
                                     <div class="flex gap-4">
                                         <a :href="route('disciplinas.edit', slotProps.data.id)"><i
-                                                class="hover:text-amber-500 pi pi-pencil"></i></a>
+                                                class="hover:text-amber-500 pi pi-pen-to-square"></i></a>
                                         <button @click="confirmDelete(slotProps.data.id)"><i
                                                 class="hover:text-red-600 pi pi-trash"></i></button>
                                     </div>
