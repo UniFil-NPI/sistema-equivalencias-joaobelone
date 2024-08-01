9cc35e682e850ea35ad68db6fa9096c9728356a7
beloner <joao.belone@edu.unifil.br>
2024-08-01 03:32:47 -0300

    removing unused dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,6 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
-        // "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
